815a9c1edf61ef01d0f78263ed40b29573a02d7c
Devin Chen <d3vin.chen@gmail.com>
2017-05-26 11:09:59 +0800

    add react-bootstrap-table basic demo

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
+var products = [{
+      id: 1,
+      name: "Item name 1",
+      price: 100
+  },{
+      id: 2,
+      name: "Item name 2",
+      price: 100
+  }];
+// It's a data format example.
+function priceFormatter(cell, row){
+  return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
+}
+
 const Root = () => (
-  <div>Hello React Hot Loader!</div>
+  <div>
+  <BootstrapTable data={products} striped={true} hover={true} bordered>
+      <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>Product ID</TableHeaderColumn>
+      <TableHeaderColumn dataField="name" dataSort={true}>Product Name</TableHeaderColumn>
+      <TableHeaderColumn dataField="price" dataFormat={priceFormatter}>Product Price</TableHeaderColumn>
+  </BootstrapTable>
+  </div>
 )
 
 export default Root
